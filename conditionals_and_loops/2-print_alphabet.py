c7269d842d90517dfb79695e93dac118b26202ec
Tessyjames <tessyjames28@gmail.com>
2025-01-14 18:41:54 +0100

    Improved the code

--- conditionals_and_loops/2-print_alphabet.py
+++ conditionals_and_loops/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # A program that prints the ASCII alphabets, in lowercase
 # not followed by a new line.
-for letters in range(97, 123):
-    print(f"{chr(letters)}", end="")
+for letter in range(97, 123):
+    print(f"{chr(letter)}", end="")
