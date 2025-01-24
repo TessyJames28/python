a84def855654f799d47d2d46b7ebaaae962bc826
Tessyjames <tessyjames28@gmail.com>
2025-01-24 21:30:01 +0100

    Print list

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Print all integers of a list
+
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print(num)
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
