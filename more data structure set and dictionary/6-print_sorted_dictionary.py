f75ac704b35043d1c82991802f896628e0bf5379
Tessyjames <tessyjames28@gmail.com>
2025-01-28 23:14:23 +0100

    Refactored code for streamlined import

--- more data structure set and dictionary/6-print_sorted_dictionary.py
+++ more data structure set and dictionary/6-print_sorted_dictionary.py
@@ -3,10 +3,13 @@
 # Function that prints a dictionary by ordered keys
 
 def print_sorted_dictionary(a_dictionary):
-    dict_keys = a_dictionary.keys()
-    sorted_dict = sorted(dict_keys)
-    for val in sorted_dict:
-        print(f"{val}: {a_dictionary[val]}")
+    if isinstance(a_dictionary, dict):
+        dict_keys = a_dictionary.keys()
+        sorted_dict = sorted(dict_keys)
+        for val in sorted_dict:
+            print(f"{val}: {a_dictionary[val]}")
 
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-print_sorted_dictionary(a_dictionary)
+
+if __name__ == "__main__":
+    a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+    print_sorted_dictionary(a_dictionary)
