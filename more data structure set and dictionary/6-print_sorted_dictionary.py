2c62490142035466d03e17b75b860029a18ef91a
Tessyjames <tessyjames28@gmail.com>
2025-01-29 21:31:06 +0100

    Returns the number of keys in a dictionary

--- more data structure set and dictionary/6-print_sorted_dictionary.py
+++ more data structure set and dictionary/6-print_sorted_dictionary.py
@@ -10,6 +10,7 @@ def print_sorted_dictionary(a_dictionary):
             print(f"{val}: {a_dictionary[val]}")
 
 
+# Avoid program execution during import
 if __name__ == "__main__":
     a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
     print_sorted_dictionary(a_dictionary)
