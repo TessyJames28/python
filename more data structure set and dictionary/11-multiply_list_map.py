f20973dd525f66dc6692e94e98bb49b01217be6d
Tessyjames <tessyjames28@gmail.com>
2025-01-31 21:52:22 +0100

    Return a list with multiplied value using map

---
+++ more data structure set and dictionary/11-multiply_list_map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# function that returns a list with all values multiplied by a number without using any loops
+
+def multiply_list_map(my_list=[], number=0):
+    multiply_list = map(lambda x: x * number, my_list)
+    return list(multiply_list)
+
+
+# Input
+my_list = [1, 2, 3, 4, 6]
+new_list = multiply_list_map(my_list, 4)
+print(new_list)
+print(my_list)
