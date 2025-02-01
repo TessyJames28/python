581e02616c381d75a10e664b597c4b4623c662ff
Tessyjames <tessyjames28@gmail.com>
2025-02-01 23:26:31 +0100

    Returns a list with a values multiplied by a number without using a loop

--- more data structure set and dictionary/11-multiply_list_map.py
+++ more data structure set and dictionary/11-multiply_list_map.py
@@ -7,7 +7,7 @@ def multiply_list_map(my_list=[], number=0):
     return list(multiply_list)
 
 
-# Input
+# Input Data
 my_list = [1, 2, 3, 4, 6]
 new_list = multiply_list_map(my_list, 4)
 print(new_list)
