256296cba5ea3e27b2ad3e7cb00206bfdbb63c0b
Tessyjames <tessyjames28@gmail.com>
2025-02-02 02:42:12 +0100

    Divides 2 integers

---
+++ exceptions/3-safe_print_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# function that divides 2 integers and prints the result
+
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print(f"Inside result: {result}")
+    return result
+
+
+# Input
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+    
