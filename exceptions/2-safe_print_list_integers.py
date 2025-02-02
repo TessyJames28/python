6abc072b5630d7a1f369de8acec9db3a085c5a39
Tessyjames <tessyjames28@gmail.com>
2025-02-02 02:18:19 +0100

    Print the first elements of a list and only integers

---
+++ exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# function that prints the first x elements of a list and only integers
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for val in range(x):
+        try:
+            print("{:d}".format(my_list[val]), end="")
+            count += 1
+        except (ValueError, TypeError, NameError):
+            continue
+    print()
+    return count
+    
+
+# Input
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
+        
