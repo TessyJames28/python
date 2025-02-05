09294dc36ea574a46c303d1239fc3d83ad4ea723
Tessyjames <tessyjames28@gmail.com>
2025-02-05 23:43:24 +0100

    Divides one list by another list

--- exceptions/4-list_division.py
+++ exceptions/4-list_division.py
@@ -25,7 +25,7 @@ def list_division(my_list_1, my_list_2, list_length):
     return new_list
 
 
-# Input
+# Input Data
 my_l_1 = [10, 8, 4]
 my_l_2 = [2, 4, 4]
 result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
