10e893bfc8dc16bbf35d4f0e50f90c7f523b7a8e
Tessyjames <tessyjames28@gmail.com>
2025-02-10 21:15:50 +0100

    An empty square

---
+++ classes and objects/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Write an empty class Square that defines a square:
+
+class Square():
+    pass
+
+
+# Input dta
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
