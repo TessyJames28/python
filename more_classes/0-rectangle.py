0ad7cac9ef091dc0445d357999b43aaaf37718ef
Tessyjames <tessyjames28@gmail.com>
2025-02-14 22:21:32 +0100

    Class Rectangle

---
+++ more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Write an empty class Rectangle that defines a rectangle:
+
+
+class Rectangle():
+    pass
+
+
+# Input Data
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
