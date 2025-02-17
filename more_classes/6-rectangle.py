6eddb9feea162e5ccdcd8ea7010b41b79948662a
Tessyjames <tessyjames28@gmail.com>
2025-02-17 20:43:13 +0100

    Added class attribute to count instances created and deleted

--- more_classes/6-rectangle.py
+++ more_classes/6-rectangle.py
@@ -88,7 +88,7 @@ class Rectangle():
 
 
 
-# Input Data
+# Input Data for testing
 my_rectangle_1 = Rectangle(2, 4)
 my_rectangle_2 = Rectangle(2, 4)
 print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
