7a59b1cf09052edda10106bdaa7381de8b1a5c71
Tessyjames <tessyjames28@gmail.com>
2025-02-22 13:36:04 +0100

    Resolved function execution upon function call

--- test_driven_development/0-add_integer.py
+++ test_driven_development/0-add_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-# Write a function that adds 2 integers.
+"""Write a function that adds 2 integers"""
 
 def add_integer(a, b=98):
+    """Function that adds two number: integers or loats"""
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     elif  not (isinstance(b, int) or isinstance(b, float)):
@@ -11,16 +12,17 @@ def add_integer(a, b=98):
         return int(a + b)
     
 
-# Input Data
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    # Input Data
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
