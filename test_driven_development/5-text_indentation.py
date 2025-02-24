7953bb461db79751b005b790b5aee7aad1c78d56
Tessyjames <tessyjames28@gmail.com>
2025-02-24 22:27:49 +0100

    Handling indentation and spacing

---
+++ test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# Write a function that prints a text with 2 new lines after each of these characters: ., ? and :
+
+def text_indentation(text):
+    """Function that prints new line based on specified characters"""
+    chars = [".", ":", "?"]
+    new_text = " ".join(text.split())
+    for char in chars:
+        new_text = new_text.replace(f"{char} ", char)
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    else:
+        for chr in new_text.strip():
+            if chr == "." or chr == ":" or chr == "?":
+                new_val = "".join(chr.strip())
+                print(new_val + "\n")
+                continue
+            print(chr, end="")
+
+
+if __name__ == "__main__":
+    # Input Data
+    text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+    Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+    Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+    Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+    rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+    stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+    cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+    beatiorem! Iam ruinas videres""")
