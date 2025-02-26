6a1d374d788a710a2aebe268bde84e455d7d34ba
Tessyjames <tessyjames28@gmail.com>
2025-02-26 20:34:12 +0100

    Still handling edge cases

--- test_driven_development/100-matrix_mul.py
+++ test_driven_development/100-matrix_mul.py
@@ -6,6 +6,8 @@
 def matrix_mul(m_a, m_b):
     """function that multiplies 2 matrices:
     """
+    cols = m_a[0]
+    rows = m_a
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     elif not isinstance(m_b, list):
@@ -14,4 +16,19 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_a can't be empty")
     elif len(m_b) == 0:
         raise ValueError("m_b can't be empty")
+    elif not isinstance(rows, list):
+        raise TypeError("m_a must be a list of lists")
+    elif not isinstance(m_a, (int, float)):
+        raise TypeError("m_a should contain only integers or floats")
+    elif not isinstance()
+    else:
+        for col in range(cols):
+            for row in range(rows)
+
+    print(f"rows: {rows}")
+    print(f"cols{cols}")
+
+matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]])
+matrix_mul([[1, 2]], [[3, 4], [5, 6]])
+
     
