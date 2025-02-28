24abb988ea63e871818cdd870a23936aa5f35f04
Tessyjames <tessyjames28@gmail.com>
2025-02-28 21:31:51 +0100

    lazy matrices multiplication using numpy library

--- test_driven_development/101-lazy_matrix_mul.py
+++ test_driven_development/101-lazy_matrix_mul.py
@@ -7,44 +7,40 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """function that multiplies 2 matrices using numpy:
     """
-    try:
-        row_length = len(m_a[0])
-    except Exception:
-        raise TypeError("m_a can't be empty")
-    
-    try:
-        row_length = len(m_b[0])
-    except Exception:
-        raise TypeError("m_b can't be empty")
-
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     elif not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    elif len(m_a) == 0 or any(len(row) == 0 for row in m_a):
+    elif len(m_a) == 0:
         raise ValueError("m_a can't be empty")
-    elif len(m_b) == 0 or any(len(row) == 0 for row in m_b):
+    elif len(m_b) == 0:
         raise ValueError("m_b can't be empty")
     else:
+        # handle edge cases for matrix a
         for row in m_a:
             if not isinstance(row, list):
                 raise TypeError("m_a must be a list of lists")
-            elif len(row) != row_length:
+            elif len(row) != len(m_a[0]):
                 raise TypeError("each row of m_a must be of the same size")
+            elif len(row) == 0:
+                raise ValueError("m_a can't be empty")
             for val in row:
                 if not isinstance(val, (int, float)):
                     raise TypeError("m_a should contain only integers or floats")
                 
-    
+        # Handle edge cases for matrix b
         for row in m_b:
             if not isinstance(row, list):
                 raise TypeError("m_b must be a list of lists")
-            elif len(row) != row_length:
+            elif len(row) != len(m_b[0]):
                 raise TypeError("each row of m_b must be of the same size")
+            elif len(row) == 0:
+                raise ValueError("m_b can't be empty")
             for val in row:
                 if not isinstance(val, (int, float)):
                     raise TypeError("m_b should contain only integers or floats")
 
+    # last edge case with matrix multiplication algo
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
     else:
@@ -58,3 +54,4 @@ def lazy_matrix_mul(m_a, m_b):
 if __name__ == "__main__":
     print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
     print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+    print(lazy_matrix_mul([], [[1, 2], [3, 4]]))
