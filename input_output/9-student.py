fe28ed2e94a528174460e59e4e3e779e0bdd7356
Tessyjames <tessyjames28@gmail.com>
2025-03-10 21:10:25 +0100

    class Student

---
+++ input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Write a class Student that defines a student by:
+"""
+
+class Student():
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a disctionary representation of a student instance"""
+        return self.__dict__
+    
+
+# Test Data
+students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+
+for student in students:
+    j_student = student.to_json()
+    print(type(j_student))
+    print(j_student['first_name'])
+    print(type(j_student['first_name']))
+    print(j_student['age'])
+    print(type(j_student['age']))
