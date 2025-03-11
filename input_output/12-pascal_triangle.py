6c03478aed344c24e3cd30372a6e9ca38cf49f74
Tessyjames <tessyjames28@gmail.com>
2025-03-11 23:54:45 +0100

    Finally solved Pascal triangle

--- input_output/12-pascal_triangle.py
+++ input_output/12-pascal_triangle.py
@@ -33,13 +33,13 @@ def pascal_triangle(n):
     return triangle
 
 
-# def print_triangle(triangle):
-#     """
-#     Print the triangle
-#     """
-#     for row in triangle:
-#         print("[{}]".format(",".join([str(x) for x in row])))
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
 
 
 if __name__ == "__main__":
-    print(pascal_triangle(5))
+    print_triangle(pascal_triangle(5))
